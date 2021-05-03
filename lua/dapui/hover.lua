73c8319d4e39e963eb075c973069c02bbce886b9
elevin72 <elilevin11@gmail.com>
2021-05-03 11:29:44 +0300

    fix(eval): hovers show content now (#8)

--- lua/dapui/hover.lua
+++ lua/dapui/hover.lua
@@ -29,7 +29,7 @@ function M.eval(expr)
         print("Couldn't evaluate expression '" .. expr .. "' in current frame.")
         return
       end
-      local val = " "..#response.result.." "
+      local val = " "..response.result.." "
       local hover_win = require("dapui.windows.float").open_float({height = 1, width = #val})
       local buf = hover_win:get_buf()
       vim.api.nvim_buf_set_lines(buf, 0, -1, true, {val})
