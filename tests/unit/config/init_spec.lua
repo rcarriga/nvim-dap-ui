133d6ea437a01c5edd3d610fd00798df9a830987
Rónán Carrigan <rcarriga@tcd.ie>
2021-07-31 09:26:13 +0100

    test: new window and config tests

--- tests/unit/config/init_spec.lua
+++ tests/unit/config/init_spec.lua
@@ -15,4 +15,22 @@ describe("checking setup function", function()
     config.setup({ icons = { expanded = "X" } })
     assert.equal(config.icons().expanded, "X")
   end)
+
+  it("fills mappings", function()
+    config.setup({ mappings = { edit = "e" } })
+    assert.same({ "e" }, config.mappings()["edit"])
+  end)
+
+  it("fills elements", function()
+    config.setup({ sidebar = { elements = { "scopes" } } })
+    assert.same({ { id = "scopes", size = 1 } }, config.sidebar().elements)
+  end)
+
+  it("fills elements with proportional size", function()
+    config.setup({ sidebar = { elements = { "scopes", "stacks" } } })
+    assert.same(
+      { { id = "scopes", size = 0.5 }, { id = "stacks", size = 0.5 } },
+      config.sidebar().elements
+    )
+  end)
 end)
