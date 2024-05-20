f5d0a215a84cdb024f7e6a527bbfb54f89d800d5
Lawrence Ho <203431+lawrenceho@users.noreply.github.com>
2024-05-20 16:35:34 +0000

    fix: add compatible tbl_flatten

--- lua/dapui/util.lua
+++ lua/dapui/util.lua
@@ -314,4 +314,9 @@ function M.format_value(value_start, value)
   return formatted
 end
 
+function M.tbl_flatten(t)
+  return vim.fn.has("nvim-0.10") == 1 and vim.iter(t):flatten(math.huge):totable()
+    or vim.tbl_flatten(t)
+end
+
 return M
