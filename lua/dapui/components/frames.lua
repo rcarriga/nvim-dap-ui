rcarriga/nvim-dap-ui

commit a62e86b124a94ad1f34a3f936ea146d00aa096d1

fix(frames): use session's frames
See #303
Closes #307

Co-authored-by: przepompownia <przepompownia@users.noreply.github.com>

lua/dapui/components/frames.lua
@@ -12,13 +12,16 @@ return function(client, send_ready)
     ---@async
     ---@param canvas dapui.Canvas
     render = function(canvas, thread_id, show_subtle, indent)
-      local success, response = pcall(client.request.stackTrace, { threadId = thread_id })
-      local current_frame_id = nil
-
-      if not success then
+      if not client.session then
         return
       end
-      local frames = response.stackFrames
+
+      local current_frame_id = nil
+
+      local frames = client.session.threads[thread_id].frames
+      if not frames then
+        return
+      end
 
       if not show_subtle then
         frames = vim.tbl_filter(function(frame)

lua/dapui/client/init.lua
@@ -19,6 +19,7 @@ local DAPUIClient = {}
 ---@field _frame_set fun(frame: dapui.types.StackFrame)
 ---@field stopped_thread_id integer
 ---@field capabilities dapui.types.Capabilities
+---@field threads table<integer, dapui.types.Thread>
 
 local proxied_session_keys = {}
 for _, key in ipairs({
@@ -26,6 +27,7 @@ for _, key in ipairs({
   "_frame_set",
   "stopped_thread_id",
   "capabilities",
+  "threads",
 }) do
   proxied_session_keys[key] = true
 end

commit 2e79267abd5f683f6311ac0bd414946c1f0c0a6b

fix: request thread stack trace if client hasn't reported it
Request thread stack trace in case Debug Adapter hasn't reported it.
This makes it possible to see stack traces for every thread when
debugging multi-threaded debugee.
Fixes regression caused by:
https://github.com/rcarriga/nvim-dap-ui/commit/a62e86b124a94ad1f34a3f936ea146d00aa096d1
Closes issue: https://github.com/rcarriga/nvim-dap-ui/issues/387

lua/dapui/components/frames.lua
@@ -26,6 +26,10 @@ return function(client, send_ready)
 
       local frames = threads[thread_id].frames
       if not frames then
+        local success, response = pcall(client.request.stackTrace, { threadId = thread_id })
+        frames = success and response.stackFrames
+      end
+      if not frames then
         return
       end
 